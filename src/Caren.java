ff105002c397290cfa1081f5637e9deb019dc378
Caren Chang <carenc@google.com>
2021-06-21 10:51:23 -0700

    Add Caren class

---
+++ src/Caren.java
@@ -0,0 +1,16 @@
+public class Caren extends EliteProgrammer {
+    @Override
+    public String getName() {
+        return "Caren";
+    }
+
+    @Override
+    public String getCurrentFavoriteSong() {
+        return "Driver's License";
+    }
+
+    @Override
+    public String getFavoriteFood() {
+        return "Sushi";
+    }
+}
