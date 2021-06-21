d6f1b4641c3af14356f4e574d2d24e282a2cc9a2
yuniskhamis <yuniskhamis3@gmail.com>
2021-06-21 14:14:44 -0400

    Added a new Java class

---
+++ src/Yunis.java
@@ -0,0 +1,18 @@
+public class Yunis extends EliteProgrammer{
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getCurrentFavoriteSong() {
+        return null;
+    }
+
+    @Override
+    public String getFavoriteFood() {
+        return null;
+    }
+
+
+}
