2d03ec4281bd6a4f4ec461099862f6df14ab8bde
shikeyaa <59205692+shikeyaa@users.noreply.github.com>
2021-06-21 15:08:10 -0400

    added my favorites file

---
+++ src/Shikeya.java
@@ -0,0 +1,18 @@
+public class Shikeya extends EliteProgrammer {
+
+
+    @Override
+    public String getName() {
+        return "Shikeya";
+    }
+
+    @Override
+    public String getCurrentFavoriteSong() {
+        return "10,000 Hours by Ella Mai";
+    }
+
+    @Override
+    public String getFavoriteFood() {
+        return "Pasta";
+    }
+}
