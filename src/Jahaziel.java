d0d882875909751bcfc3cbf1926822da3ca2d33c
Jahaziel Israel <68349525+blaquebeardcoder@users.noreply.github.com>
2021-06-21 14:02:46 -0400

    Adding Jahaziel.java class

---
+++ src/Jahaziel.java
@@ -0,0 +1,17 @@
+public class Jahaziel extends EliteProgrammer{
+
+    @Override
+    public String getName() {
+        return "Jahaziel";
+    }
+
+    @Override
+    public String getCurrentFavoriteSong() {
+        return "Spiritual State ft. Uyama Hiroto by Nujabes";
+    }
+
+    @Override
+    public String getFavoriteFood() {
+        return "Thai";
+    }
+}
