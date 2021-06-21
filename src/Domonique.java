1c0f6a1c8a613fbc24b798ad63afe2d0ab6eef2d
MissChurchill <misschurchill.dc@gmail.com>
2021-06-21 15:50:54 -0400

    Commit File Domonique

---
+++ src/Domonique.java
@@ -0,0 +1,14 @@
+public class Domonique extends EliteProgrammer {
+
+    public String getName() {
+        return "Domonique";
+    }
+
+    public String getCurrentFavoriteSong() {
+        return "Look Easy";
+    }
+
+    public String getFavoriteFood() {
+        return "hibachi and sushi";
+    }
+}
