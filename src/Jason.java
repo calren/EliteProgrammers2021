5eac83a53f06782708d491b4f948634ad4cb3c79
Jason Williamson <Apex2673@gmail.com>
2021-06-22 08:46:05 -0500

    Added Jason Class

---
+++ src/Jason.java
@@ -0,0 +1,16 @@
+public class Jason extends EliteProgrammer {
+    @Override
+    public String getName() {
+        return "Jason";
+    }
+
+    @Override
+    public String getCurrentFavoriteSong() {
+        return "So What";
+    }
+
+    @Override
+    public String getFavoriteFood() {
+        return "Salmon";
+    }
+}
