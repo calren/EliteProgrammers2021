2ad97c5ac50858d21c40d47073f00cc767278983
williamsonb <43324783+williamsonb@users.noreply.github.com>
2021-06-27 16:24:52 -0500

    Initial commit

---
+++ src/Bryan.java
@@ -0,0 +1,17 @@
+public class Bryan extends EliteProgrammer{
+
+    @Override
+    public String getName() {
+        return "Bryan";
+    }
+
+    @Override
+    public String getCurrentFavoriteSong() {
+        return "As";
+    }
+
+    @Override
+    public String getFavoriteFood() {
+        return "Curry";
+    }
+}
